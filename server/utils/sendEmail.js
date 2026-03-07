f8027e3217a0fcff5fbc1b44a98d2c45cda9072e
nadhafathima281-star <nadhafathima281@gmail.com>
2026-03-07 12:12:26 +0530

    fix email transporter

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -5,13 +5,18 @@ const nodemailer=require('nodemailer')
 
 
 //create reusable transporter
-const transporter=nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:process.env.EMAIL_USER,
-        pass:process.env.EMAIL_PASS,
-    },
-})
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
 
 // verify transporter on server start
 transporter.verify((error,success)=>{
