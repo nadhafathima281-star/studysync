b12fad03e1b4298344aaf09171e63e45970dcb63
nadhafathima281-star <nadhafathima281@gmail.com>
2026-03-07 12:04:24 +0530

    fix cors for vercel

--- server/server.js
+++ server/server.js
@@ -22,10 +22,7 @@ const app = express();
 app.use(
   cors({
     
-    origin: [
-      "http://localhost:5173",
-      "https://studysync-ecru.vercel.app"
-    ],
+    origin:true,
     credentials: true,
   })
 );
